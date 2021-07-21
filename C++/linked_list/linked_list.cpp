a7e109b59a3b20f2b4ca6544d22a5f7857b8e9ed
frankjr12 <franklinjunior12@hotmail.com>
2021-07-20 21:45:26 -0300

    added remove and count in linked list

--- C++/linked_list/linked_list.cpp
+++ C++/linked_list/linked_list.cpp
@@ -1,5 +1,11 @@
 /*
  * Simple linked list with loose end (final node points to NULL).
+ * Todo:
+ * Add node to list (OK)
+ * Print list (OK)
+ * Remove node from list (OK)
+ * Count number of elements ()
+ * Sort list by id ()
  */
 
 #include <stdio.h>
@@ -21,11 +27,12 @@ int ll_node_print (ll_node* p, void (*f)(void*)) {
     int ret = 0;
     ll_node* next = p;
     while (next != NULL) {
-        printf ("id = %d\n", next->id);
+        printf (" %d ->", next->id);
         if (f != NULL) 
             f(next->sp);
         next = next->next;
     }
+    printf("\n");
     return ret;
 }
 
@@ -41,6 +48,34 @@ int ll_node_add (ll_node* first, ll_node* second) {
     return ret;
 }
 
+int ll_node_remove (ll_node* first, ll_node* n) {
+    if (first == NULL)
+        return -1;
+    int ret = 0;
+    ll_node* current = first;
+    while (current->next != n && current != NULL) {
+        current = current->next;        
+    }
+    if (current == NULL)
+        return -1;
+    ll_node* next = current->next->next;
+    current->next->next = NULL;
+    current->next = next;
+    return ret;
+}
+
+
+int ll_node_count_elements (ll_node* first, unsigned int* output) {
+    if (first == NULL || (*output) != 0)
+        return -1;    
+    int ret = 0;
+    ll_node* current = first;
+    while (current != NULL) {
+        current = current->next;
+        (*output)++;
+    }
+    return ret;
+}
 
 void bla_print (void* b) {
     bla* a = (bla*)b;
@@ -69,12 +104,27 @@ int main (void) {
         .next = NULL,
         .sp = (void*) &b
     };
+    
+   ll_node node3 = {
+        .id = counter++,
+        .next = NULL,
+        .sp = (void*) &b
+    };
+    
 
 
     // ll_node_print(&node1, bla_print);
     ll_node_print(&node1, NULL);
 
     ll_node_add(&node1, &node2);
+    ll_node_add(&node1, &node3);
+
+    ll_node_print(&node1, NULL);
+    unsigned int len = 0;    
+    ll_node_count_elements(&node1, &len);
+    printf("Number of elements: %u\n", len);
+
+    ll_node_remove(&node1, &node2);
 
     ll_node_print(&node1, NULL);
 
